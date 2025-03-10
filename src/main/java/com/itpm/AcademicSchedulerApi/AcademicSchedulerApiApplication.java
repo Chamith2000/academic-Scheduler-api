a3757d6dcc31e47c22c65a655de25c21f1363a58
Chamith2000 <chamithrathdunu38@gmail.com>
2025-03-10 23:27:39 +0530

    Create folder structure

---
+++ src/main/java/com/itpm/AcademicSchedulerApi/AcademicSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package com.itpm.AcademicSchedulerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itpm/AcademicSchedulerApi/AcademicSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itpm.AcademicSchedulerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
