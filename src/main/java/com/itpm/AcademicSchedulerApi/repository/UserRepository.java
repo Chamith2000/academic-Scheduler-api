dd1d683d6c68c1dff57c7994ef4ef3bffe37b3fb
Chamith2000 <chamithrathdunu38@gmail.com>
2025-03-24 21:24:13 +0530

    create reset controller

--- src/main/java/com/itpm/AcademicSchedulerApi/repository/UserRepository.java
+++ src/main/java/com/itpm/AcademicSchedulerApi/repository/UserRepository.java
@@ -1,8 +1,7 @@
 package com.itpm.AcademicSchedulerApi.repository;
 
-import org.apache.catalina.User;
+import com.itpm.AcademicSchedulerApi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
